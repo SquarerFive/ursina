f81cea907b16299bb614326a7c9ee6d0d37f061c
SquarerFive <aidensoedjarwo@gmail.com>
2020-03-29 12:21:38 +1100

    Update camera.py

--- ursina/camera.py
+++ ursina/camera.py
@@ -31,7 +31,7 @@ class Camera(Entity):
     def set_up(self):
         self.display_region = base.camNode.get_display_region(0)
         print(f"Display region: {self.display_region}")
-        print(f"regions: {win.getDisplayRegions()}")
+        print(f"regions: {base.win.getDisplayRegions()}")
         win = self.display_region.get_window()
 
         self.perspective_lens = PerspectiveLens()
